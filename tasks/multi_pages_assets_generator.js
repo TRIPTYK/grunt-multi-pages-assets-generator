7975d3fd5cd91910f83ea743e4af2904190909d9
thecrazydigitalpainter <gilles@Mac-Pro-de-gilles.local>
2015-03-17 13:50:27 +0100

    last version

--- tasks/multi_pages_assets_generator.js
+++ tasks/multi_pages_assets_generator.js
@@ -13,7 +13,6 @@ var uglifyCSS =require('uglifycss');
 module.exports = function(grunt) {
 
   function generate(options, destinationFolder,srcFolder) {
-
     //Before startup check for the requirements
     if (!grunt.file.exists(options.configJSON)) grunt.fail.warn("There should be a config.json file at this path " + options.configJSON, 1);
     if (!grunt.file.exists(options.cssFolder)) grunt.fail.warn("There should be a css folder at this path " + options.cssFolder, 2);
@@ -21,7 +20,7 @@ module.exports = function(grunt) {
     var pages = grunt.file.readJSON(options.configJSON).pages;
     pages.map(function(item) {
         var cssArr = item.css.map(function(inItem){return options.cssFolder+'/'+inItem});
-        var jsArr = item.jsComponents.map(function(inItem){return options.jsComponents+'/'+inItem}).concat(item.js.map(function(inItem){return options.jsFolder+'/'+inItem}));
+        var jsArr = item.js.map(function(inItem){return options.jsFolder+'/'+inItem});
         var fileName = item.idName;
         grunt.file.mkdir(destinationFolder);
         grunt.file.mkdir(destinationFolder + '/css');
@@ -31,8 +30,7 @@ module.exports = function(grunt) {
         grunt.file.write(destinationFolder+'/css/'+fileName+'.min.css', finalCss );
         grunt.file.write(destinationFolder+'/js/'+fileName+'.min.js', finalJs.code );
         var destpage = item.page.replace(srcFolder,'');
-       grunt.file.copy(item.page, destinationFolder+'/'+destpage)
-
+        grunt.file.copy(item.page, destinationFolder+'/'+destpage)
     });
   }
 
@@ -46,16 +44,15 @@ module.exports = function(grunt) {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       jsFolder: this.jsFolder,
-      jsComponents : this.jsComponents,
       cssFolder: this.cssFolder,
       configJSON: this.configJSON
     });
+
     // Iterate over all specified file groups.
     // this.files.forEach(function(f) {
     //   grunt.log.writeln('File de merde "' + f.dest + '" created.');
     // });
     generate(options, this.files[0].dest, this.filesSrc);
-
   });
 
 };
